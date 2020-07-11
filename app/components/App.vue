89cfc251ec1174e71b1039b9ce6301e54d691f1a
reybel24 <reybel24@gmail.com>
2020-07-11 01:33:05 -0400

    style bottom navigation

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,29 +1,28 @@
 <template lang="html">
     <BottomNavigation class="BottomNavigation">
+        <!-- Navigation -->
         <TabStrip>
             <TabStripItem class="navigation__item">
-                <Label text="Home"></Label>
-                <Image src.decode="font://&#xf015;" class="fas t-36"></Image>
+                <Label :text="String.fromCharCode(0xf015)" class="fas icon" />
             </TabStripItem>
             <TabStripItem class="navigation__item">
-                <Label text="Browse"></Label>
-                <Image src.decode="font://&#xf1ea;" class="far t-36"></Image>
+                <Label :text="String.fromCharCode(0xf002)" class="fas icon" />
             </TabStripItem>
             <TabStripItem class="navigation__item">
-                <Label text="Search"></Label>
-                <Image src.decode="font://&#xf002;" class="fas t-36"></Image>
+                <Label :text="String.fromCharCode(0xf54e)" class="fas icon" />
+            </TabStripItem>
+            <TabStripItem class="navigation__item">
+                <Label :text="String.fromCharCode(0xf004)" class="fas icon" />
+            </TabStripItem>
+            <TabStripItem class="navigation__item">
+                <Label :text="String.fromCharCode(0xf2bd)" class="fas icon" />
             </TabStripItem>
         </TabStrip>
 
+        <!-- Views -->
         <TabContentItem>
             <Frame id="items">
                 <Items/>
-            </Frame>
-        </TabContentItem>
-
-        <TabContentItem>
-            <Frame id="browse">
-                <Browse/>
             </Frame>
         </TabContentItem>
 
@@ -33,31 +32,65 @@
             </Frame>
         </TabContentItem>
 
+        <TabContentItem>
+            <Frame id="search">
+                <Browse/>
+            </Frame>
+        </TabContentItem>
+
+        <TabContentItem>
+            <Frame id="browse">
+                <Search/>
+            </Frame>
+        </TabContentItem>
+
+        <TabContentItem>
+            <Frame id="watched">
+                <Search/>
+            </Frame>
+        </TabContentItem>
+
+        <TabContentItem>
+            <Frame id="profile">
+                <Search/>
+            </Frame>
+        </TabContentItem>
     </BottomNavigation>
 </template>
 
 <script>
-  import Items from "./Items.vue";
-  import Browse from "./Browse.vue";
-  import Search from "./Search.vue";
+import Items from "./Items.vue";
+import Browse from "./Browse.vue";
+import Search from "./Search.vue";
 
-  export default {
-    components: {
-      Items,
-      Browse,
-      Search
-    }
-  };
+export default {
+  components: {
+    Items,
+    Browse,
+    Search
+  },
+  methods: {
+      switchTab(tab) {
+          console.log("Navigatsing");
+          this.$navigateTo(Browse);
+      },
+      sampleHome() {
+          console.log("going home")
+      }
+  }
+};
 </script>
 
 <style lang="scss">
-    // Start custom common variables
-    @import "~@nativescript/theme/scss/variables/blue";
-    // End custom common variables
+// Start custom common variables
+@import "~@nativescript/theme/scss/variables/blue";
+// End custom common variables
 
 // Custom styles
-.BottomNavigation {
-    background-color: blue;
+TabStrip {
+    background-color: rgb(255, 255, 255);
+    selected-item-color: #ff596f;
+    un-selected-item-color: #6c678e;;
+    highlight-color: green;
 }
-
 </style>
